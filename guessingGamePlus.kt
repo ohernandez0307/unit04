29dd9e0accfda0c2950d487aae1ab4f31008b353
ohernandez0307 <123129470+ohernandez0307@users.noreply.github.com>
2023-03-09 21:57:45 -0600

    Add files via upload

---
+++ guessingGamePlus.kt
@@ -0,0 +1,31 @@
+import kotlin.random.Random
+
+fun main() {
+    val rand = Random
+    val max = 10
+    var guess: Int
+    var randNum: Int
+    var count = 0
+
+    randNum = rand.nextInt(max)
+
+    print("Guess a random number: ")
+    guess = readln().toInt()
+
+    count = 1
+
+    while (guess != randNum) {
+        if (guess < randNum) {
+            println("That's too low")
+        } else if (guess > randNum) {
+            println("That's too high")
+        }
+
+        print("Guess again: ")
+        guess = readln().toInt()
+
+        count++
+    }
+    println("You guessed correctly!")
+    println("It took you $count times to guess the correct number: $randNum")
+}
